82869d6e243d8cc0c9118497d0e181760a9d52c0
ijeffro <phil.graham@ht2labs.com>
2019-05-15 23:25:05 +0100

    wip

--- src/Providers/LaralockerServiceProvider.php
+++ src/Providers/LaralockerServiceProvider.php
@@ -21,7 +21,7 @@ class LaralockerServiceProvider extends ServiceProvider
 
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/laralocker.php' => config_path('laralocker.php'),
+                __DIR__.'/../config/config.php' => config_path('laralocker.php'),
             ], 'config');
 
             // Publishing the views.
@@ -50,7 +50,7 @@ class LaralockerServiceProvider extends ServiceProvider
     public function register()
     {
         // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/laralocker.php', 'laralocker');
+        $this->mergeConfigFrom(__DIR__.'/../../config/laralocker.php', 'laralocker');
 
         // Register the main class to use with the facade
         $this->app->singleton('laralocker', function () {
